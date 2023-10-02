b03b1a16e2a066458a4dc7a7a4a2b0c99c0e37cf
Ankittny <tny3infotech@gmail.com>
2023-10-02 11:21:06 +0530

    comming shoon

---
+++ package.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light">
+
+<head>
+  <title>test</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Sites meta Data -->
+  <meta name="application-name"
+    content="Bitrader - Professional Multipurpose HTML Template for Your Crypto, Forex, and Stocks Trading Business">
+  <meta name="author" content="thetork">
+  <meta name="keywords" content="Bitrader, Crypto, Forex, and Stocks Trading Business">
+  <meta name="description"
+    content="Experience the power of Bitrader, the ultimate HTML template designed to transform your trading business. With its sleek design and advanced features, Bitrader empowers you to showcase your expertise, engage clients, and dominate the markets. Elevate your online presence and unlock new trading possibilities with Bitrader.">
+
+  <!-- OG meta data -->
+  <meta property="og:title"
+    content="Bitrader - Professional Multipurpose HTML Template for Your Crypto, Forex, and Stocks Trading Business">
+  <meta property="og:site_name" content=Bitrader>
+  <meta property="og:url" content="index.html">
+  <meta property="og:description"
+    content="Welcome to Bitrader, the game-changing HTML template meticulously crafted to revolutionize your trading business. With its sleek and modern design, Bitrader provides a cutting-edge platform to showcase your expertise, attract clients, and stay ahead in the competitive trading markets.">
+  <meta property="og:type" content="website">
+  <meta property="og:image" content="assets/images/og.png">
+
+
+
+  <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/aos.css">
+  <link rel="stylesheet" href="assets/css/all.min.css">
+
+  <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+
+
+
+  <!-- main css for template -->
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+  <!-- ===============>> Preloader start here <<================= -->
+  <div class="preloader">
+    <img src="assets/images/logo/preloader.png" alt="preloader icon">
+  </div>
+  <!-- ===============>> Preloader end here <<================= -->
+
+
+
+  <!-- ===============>> light&dark switch start here <<================= -->
+  <div class="lightdark-switch d-none">
+    <span class="dark-btn" id="btnSwitch"><img src="assets/images/icon/moon.svg" alt="light-dark-switchbtn"
+        class="swtich-icon"></span>
+  </div>
+  <!-- ===============>> light&dark switch start here <<================= -->
+
+
+
+
+
+  <!-- ===============>> Header section start here <<================= -->
+  <header class="header-section header-section--style2">
+    <div class="header-bottom">
+      <div class="container">
+        <div class="header-wrapper">
+          <div class="logo">
+            <a href="index-2.html">
+              <img class="dark" src="assets/images/logo/logo.png" alt="logo">
+            </a>
+          </div>
+          <div class="menu-area">
+           
+
+          </div>
+          <div class="header-action">
+            <div class="menu-area">
+              <div class="header-btn">
+               
+              </div>
+
+              <!-- toggle icons -->
+              <div class="header-bar d-lg-none header-bar--style1">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- ===============>> Header section end here <<================= -->
+
+
+
+
+
+  <!-- ===============>> Pricing section start here <<================= -->
+  <section class="pricing padding-top padding-bottom">
+    <div class="section-header section-header--max50">
+      <h2 class="mb-15 mt-minus-5">Our <span>pricings </span>plan</h2>
+      <p>We offer the best pricings around - from installations to repairs, maintenance, and more!</p>
+    </div>
+    <div class="container">
+      <div class="pricing__wrapper">
+        <div class="row g-4 align-items-center">
+          <div class="col-md-6 col-lg-4">
+            <div class="pricing__item" data-aos="fade-right" data-aos-duration="1000">
+              <div class="pricing__item-inner">
+                <div class="pricing__item-content">
+
+                  <!-- pricing top part -->
+                  <div class="pricing__item-top">
+                    <h6 class="mb-15">BRONZE</h6>
+                    <h3 class="mb-25">₹ 3K/<span>Monthly</span> </h3>
+                  </div>
+
+                  <!-- pricing middle part -->
+                  <div class="pricing__item-middle">
+                    <ul class="pricing__list">
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span>Bluetooth Speaker</li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Smart Watch </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Ear bud</li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Electric Kettle  </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Hair Dryer </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Bag </li>
+                    </ul>
+
+                  </div>
+
+                  <!-- pricing bottom part -->
+                  <div class="pricing__item-bottom">
+                    <a href="signup.html" class="trk-btn trk-btn--outline">Choose Plan</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="pricing__item" data-aos="fade-right" data-aos-duration="1000">
+              <div class="pricing__item-inner">
+                <div class="pricing__item-content">
+
+                  <!-- pricing top part -->
+                  <div class="pricing__item-top">
+                    <h6 class="mb-15">SILVER</h6>
+                    <h3 class="mb-25">₹ 6K/<span>Monthly</span> </h3>
+                  </div>
+
+                  <!-- pricing middle part -->
+                  <div class="pricing__item-middle">
+                    <ul class="pricing__list">
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check"  class="dark"></span> Home Theater </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Ear bud  </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Smart Watch</li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Hair Dryer </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Electric Kettle  </li>
+                     
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Bag </li>
+                    </ul>
+
+                  </div>
+
+                  <!-- pricing bottom part -->
+                  <div class="pricing__item-bottom">
+                    <a href="signup.html" class="trk-btn trk-btn--outline">Choose Plan</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        
+          <div class="col-md-6 col-lg-4">
+            <div class="pricing__item" data-aos="fade-left" data-aos-duration="1000">
+              <div class="pricing__item-inner">
+                <div class="pricing__item-content">
+
+                  <!-- pricing top part -->
+                  <div class="pricing__item-top">
+                    <h6 class="mb-15">GOLD</h6>
+                    <h3 class="mb-25">₹ 9K/<span>Monthly</span> </h3>
+                  </div>
+
+                  <!-- pricing middle part -->
+                  <div class="pricing__item-middle">
+                    <ul class="pricing__list">
+                    <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check"  class="dark"></span>RO Water Purifier </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Home Theater </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Smart Watch</li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Ear buds </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Hair Dryer  </li>
+                     
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Bag </li>
+                    </ul>
+
+                  </div>
+
+                  <!-- pricing bottom part -->
+                  <div class="pricing__item-bottom">
+                    <a href="signup.html" class="trk-btn trk-btn--outline">Choose Plan</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="pricing__item" data-aos="fade-left" data-aos-duration="1000">
+              <div class="pricing__item-inner">
+                <div class="pricing__item-content">
+
+                  <!-- pricing top part -->
+                  <div class="pricing__item-top">
+                    <h6 class="mb-15">PLATINUM</h6>
+                    <h3 class="mb-25">₹ 11K/<span>Monthly</span> </h3>
+                  </div>
+
+                  <!-- pricing middle part -->
+                  <div class="pricing__item-middle">
+                    <ul class="pricing__list">
+                    <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check"  class="dark"></span>32” Android Smart LED </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Smart Watch  </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Ear buds</li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> BT Speaker </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span>Electric Kettle  </li>
+                     
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Bag </li>
+                    </ul>
+
+                  </div>
+
+                  <!-- pricing bottom part -->
+                  <div class="pricing__item-bottom">
+                    <a href="signup.html" class="trk-btn trk-btn--outline">Choose Plan</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="pricing__item" data-aos="fade-left" data-aos-duration="1000">
+              <div class="pricing__item-inner">
+                <div class="pricing__item-content">
+
+                  <!-- pricing top part -->
+                  <div class="pricing__item-top">
+                    <h6 class="mb-15">DIAMOND </h6>
+                    <h3 class="mb-25">₹ 17K/<span>Monthly</span> </h3>
+                  </div>
+
+                  <!-- pricing middle part -->
+                  <div class="pricing__item-middle">
+                    <ul class="pricing__list">
+                    <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check"  class="dark"></span>43” Android Smart 4K LED </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Smart Watch  </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Sound Bar</li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Ear buds </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span>Electric Kettle  </li>
+                     
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Bag </li>
+                    </ul>
+
+                  </div>
+
+                  <!-- pricing bottom part -->
+                  <div class="pricing__item-bottom">
+                    <a href="signup.html" class="trk-btn trk-btn--outline">Choose Plan</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="pricing__item" data-aos="fade-left" data-aos-duration="1000">
+              <div class="pricing__item-inner">
+                <div class="pricing__item-content">
+
+                  <!-- pricing top part -->
+                  <div class="pricing__item-top">
+                    <h6 class="mb-15">Emerald </h6>
+                    <h3 class="mb-25">₹ 30K/<span>Monthly</span> </h3>
+                  </div>
+
+                  <!-- pricing middle part -->
+                  <div class="pricing__item-middle">
+                    <ul class="pricing__list">
+                    <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check"  class="dark"></span>50” Android Smart 4K LED </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Home theater  </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Smart Watch</li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Ear buds </li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span>Hair Dryer</li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span>Electric Kettle</li>
+                      <li class="pricing__list-item"><span><img src="assets/images/icon/check.svg" alt="check" class="dark"></span> Bag </li>
+                    </ul>
+
+                  </div>
+
+                  <!-- pricing bottom part -->
+                  <div class="pricing__item-bottom">
+                    <a href="signup.html" class="trk-btn trk-btn--outline">Choose Plan</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="pricing__shape">
+      <span class="pricing__shape-item pricing__shape-item--1"> <span></span> </span>
+      <span class="pricing__shape-item pricing__shape-item--2"> <img src="assets/images/icon/1.png" alt="shape-icon">
+      </span>
+    </div>
+  </section>
+  <!-- ===============>> Pricing section start here <<================= -->
+
+
+
+
+
+
+
+
+  
+
+
+  <!-- ===============>> scrollToTop start here <<================= -->
+  <a href="#" class="scrollToTop scrollToTop--home1"><i class="fa-solid fa-arrow-up-from-bracket"></i></a>
+  <!-- ===============>> scrollToTop ending here <<================= -->
+
+
+  <!-- vendor plugins -->
+
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/all.min.js"></script>
+  <script src="assets/js/swiper-bundle.min.js"></script>
+  <script src="assets/js/aos.js"></script>
+  <script src="assets/js/fslightbox.js"></script>
+  <script src="assets/js/purecounter_vanilla.js"></script>
+
+
+
+  <script src="assets/js/custom.js"></script>
+
+
+</body>
+
+
+<!-- Mirrored from thetork.com/demos/html/bitrader/index1-light.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 01 Oct 2023 06:25:34 GMT -->
+</html>
